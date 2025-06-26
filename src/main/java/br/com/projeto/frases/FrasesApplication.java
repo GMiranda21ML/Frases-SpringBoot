2123525ab0ca3fccafd253a82a09eb938b0305e5
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-06-26 17:55:15 -0300

    criando a tabela de frases

--- src/main/java/br/com/projeto/frases/FrasesApplication.java
+++ src/main/java/br/com/projeto/frases/FrasesApplication.java
@@ -5,14 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrasesApplication implements CommandLineRunner {
+public class FrasesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrasesApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
-	}
 }
---
+++ src/main/java/br/com/projeto/frases/model/Frase.java
@@ -0,0 +1,42 @@
+package br.com.projeto.frases.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "frases")
+public class Frase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String frase;
+    private String personagem;
+    private String titulo;
+    private String poster;
+
+    public Frase(String frase, String personagem, String titulo, String poster) {
+        this.frase = frase;
+        this.personagem = personagem;
+        this.titulo = titulo;
+        this.poster = poster;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public String getPersonagem() {
+        return personagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+}
